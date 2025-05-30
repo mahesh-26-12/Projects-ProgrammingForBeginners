b0f17b57a374d3904a61cf98511b16f69f49bc6a
mahesh-26-12 <pudarimaheshffn@gmail.com>
2025-05-30 21:12:07 +0530

    Add files via upload
    toast notification program added

---
+++ 17_toastNotification/script.js
@@ -0,0 +1,25 @@
+let buttons = document.querySelectorAll('.container button')
+let toastBox = document.querySelector(' #toastBox')
+
+buttons.forEach(button => {
+    button.addEventListener('click',() =>{
+        let toast = document.createElement('div')
+        if(button.innerHTML === 'Success')
+        toast.innerHTML = 'success'
+        if(button.innerHTML === 'Error'){
+            toast.innerHTML = 'Error'
+            toast.classList.add('error')
+
+        }
+        if(button.innerHTML === 'Invalid'){
+            toast.innerHTML = 'Invalid'
+             toast.classList.add('invalid')
+        }
+        toast.classList.add('toast')
+        toastBox.appendChild(toast)
+
+        setTimeout(() => {
+            toast.remove()
+        },5000)
+    })
+})
