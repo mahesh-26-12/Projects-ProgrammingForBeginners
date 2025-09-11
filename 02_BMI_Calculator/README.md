5d2466d2170f411ae8d522688a4a0673b3b900c7
Mahesh <pudarimaheshff@gmail.com>
2025-09-11 13:56:27 +0530

    Added Reset button and category diplay

---
+++ 02_BMI_Calculator/README.md
@@ -0,0 +1,30 @@
+BMI Calculator
+
+A simple and responsive **BMI (Body Mass Index) Calculator** built using **HTML,CSS and Javascript**.
+This project helps users calculate BMI based on weight and height, and also categorizes them into standard BMI ranges.
+
+
+## Features
+- Input fields for **Weight (kg)** and **Height (cm)**.
+-**BMI Calculation** using the standard formula:
+BMI = weight(kg) / (height(m)*height(m))
+-**Validation**: Ensures valid weight and height values are entered.
+-**BMI Category**:
+-underweight (BMI < 18.5)
+-Normal weight (18.5 ≤ BMI < 24.9  )
+-Overweight (25 ≤ BMI < 29.9)
+-Obese (BMI ≥ 30)
+-**Reset button** to clear all fields and results.
+
+
+#Technologies Used
+-**HTML5** -> Structure of the calculator
+-**CSS3** -> Styling and layout
+-**JavaScript** -> BMI calculation logic and interactivity
+
+## Project Structure
+BMI-Calculator/
+|----index.html #Main HTML file
+|----style.css #CSS file for styling
+|----script.js #Javascript file with logic
+|----README.md #Documentation
--- 02_BMI_Calculator/script.js
+++ 02_BMI_Calculator/script.js
@@ -1,29 +1,57 @@
 
-let btnCalculate = document.querySelector('#calculateBMI')
-let result = document.querySelector('#result')
+let btnCalculate = document.querySelector('#calculateBMI');
+let btnReset = document.querySelector('#reset');
+let result = document.querySelector('#result');
+let category = document.querySelector('#category')
 
 //BMI = weight (kg) / (height * height) 
 
 function calculateBMI(){
-    let weight = document.querySelector('#weight')
-    let height = document.querySelector('#height')
-    weight = parseInt(weight.value)
-    height = parseInt(height.value)
-     if(weight === '' ){
+    let weight = document.querySelector('#weight').value;
+    let height = document.querySelector('#height').value;
+    
+    //Validation
+    
+     if(weight === '' || isNaN(weight) || weight <= 0 ){
         alert('Please provide valid weight in kg')
-        return
+        return null;
      }
-     if(height === '' || isNaN(height)){
+     if(height === '' || isNaN(height) || height <= 0){
         alert('Please provide valid height in cm')
-        return
+        return null;
      }
+
+     //Convert height to meters
      height = height/100
      let bmi = weight/(height * height)
+
      return bmi.toFixed(2)
 
 }
 
+//Function to determine BMI Category
+function getBMICategory(bmi){
+   if(bmi < 18.5) return "Underweight";
+   else if (bmi >= 18.5 && bmi < 24.9) return "Normal weight";
+   else if (bmi >= 25 && bmi < 29.9) return "Overweight";
+   else return "Obese";
+}
+
+//Event listener for Calculate 
+
 btnCalculate.addEventListener('click',function(){
-    let bmi = calculateBMI()
-    result.innerHTML = `BMI = ${bmi}`
+    let bmi = calculateBMI();
+    if(bmi) {
+      result.innerHTML = `Your BMI = <strong>${bmi}</strong>`;
+      category.innerHTML = `Category: <strong>${getBMICategory(bmi)}</strong>`;
+    }
+    
+});
+
+//Event listener for Reset 
+btnReset.addEventListener('click',function(){
+   document.querySelector('#weight').value = "";
+   document.querySelector('#height').value = "";
+   result.innerHTML = "Result will display here";
+   category.innerHTML = "";
 })
